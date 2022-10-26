bd1d7d8f0b998769b6eea87345cc4e54024c84b9
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-10-26 00:10:40 -0300

    feat: add model of article and source

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,6 +1,41 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class Article extends Equatable {
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String content;
+
+  const Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.content,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      content: json['content'],
+    );
+  }
+
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        content,
+      ];
 }
--- lib/models/source_model.dart
+++ lib/models/source_model.dart
@@ -1,7 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class Source extends Equatable {
+  final String id;
+  final String name;
+
+  const Source({
+    required this.id,
+    required this.name,
+  });
+
+  factory Source.fromJson(Map<String, dynamic> json) {
+    return Source(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+
   @override
-  // TODO: implement props
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        id,
+        name,
+      ];
 }
